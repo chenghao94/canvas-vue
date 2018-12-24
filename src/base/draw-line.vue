00327fd0111996740596929f8df5eee3a347fc53
chenghao <chenghao01@360.net>
2018-12-24 18:35:05 +0800

    draw line

--- src/base/draw-line.vue
+++ src/base/draw-line.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <canvas ref="canvas" width="800" height="600"></canvas>
+      <canvas ref="canvas" :width="width" :height="height"></canvas>
     </div>
 </template>
 
@@ -10,14 +10,36 @@ export default {
   components: {},
   data () {
     return {
+      width: 0,
+      height: 0,
       ctx: {}
     }
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    drawLine () {
+      this.ctx.clearRect(0, 0, this.width, this.height)
+      this.ctx.lineWidth = 3
+      this.ctx.lineCap = 'round'
+      this.ctx.beginPath()
+      this.ctx.moveTo(0, 0)
+      this.ctx.bezierCurveTo(150, 200, 350, 300, 600, 300)
+      // 创建线性渐变
+      let gradient = this.ctx.createLinearGradient(0, 0, 250, 250)
+      gradient.addColorStop(0.2, 'blue')
+      gradient.addColorStop(0.8, 'red')
+      this.ctx.strokeStyle = gradient
+      this.ctx.stroke()
+    }
+  },
   mounted () {
+    this.width = this.$refs.canvas.parentNode.getBoundingClientRect().width
+    this.height = this.$refs.canvas.parentNode.getBoundingClientRect().height
     this.ctx = this.$refs.canvas.getContext('2d')
+    this.$nextTick(() => {
+      this.drawLine()
+    })
   },
   props: {}
 }
