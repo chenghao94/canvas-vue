f59d1775eb77cb9ec7bcd1d2503acf3f0d7b0a53
chenghao <chenghao01@360.net>
2019-07-08 11:00:13 +0800

    构思

---
+++ src/force-graph/force-graph.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>force-graph</div>
+</template>
+
+<script>
+export default {
+  name: 'force-graph',
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  mounted () {
+  },
+  props: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -9,6 +9,11 @@ export default new Router({
       path: '/',
       name: '组件连线',
       component: () => import('./page/drag-link/index.vue')
+    },
+    {
+      path: '/test',
+      name: '测试',
+      component: () => import('./force-graph/test.vue')
     }
   ]
 })
