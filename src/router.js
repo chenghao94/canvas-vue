db45abd52e83902f8ddab40ac9fc5b6ba59a4815
chenghao01 <chenghao01@360.net>
2020-01-20 15:30:46 +0800

    提交一波代码

--- src/router.js
+++ src/router.js
@@ -9,25 +9,6 @@ export default new Router({
       path: '/',
       name: '组件连线',
       component: () => import('./page/drag-link/index.vue')
-    },
-    {
-      path: '/test',
-      name: '测试',
-      component: () => import('./force-graph/test.vue')
-    },
-    {
-      path: '/path1',
-      name: '测试',
-      component: () => import('./page/svg-path/path1.vue')
-    },
-    {
-      path: '/path2',
-      name: '测试',
-      component: () => import('./page/svg-path/path2.vue')
-    },
-    {
-      path: '/layout',
-      name: '菜单',
     }
   ]
 })
